0fae73877beccac407d5b2ff585fb3923978b0fa
Eder <eferreira@feelfreerentals.com>
2018-04-06 12:16:06 +0200

    Eliminada función innecesaria

--- src/AppBundle/Repository/ClientRepository.php
+++ src/AppBundle/Repository/ClientRepository.php
@@ -3,11 +3,4 @@ namespace AppBundle\Repository;
 use Doctrine\ORM\EntityRepository;
 class ClientRepository extends EntityRepository
 {
-    
-    public function listAction()
-{
-    $client = $this->getDoctrine()
-        ->getRepository(Client::class)
-        ->findAllOrderedByName();
-}
 }
